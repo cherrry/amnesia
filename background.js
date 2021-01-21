3b4eb58a246e98b566b753e55551e67bdf85bd50
Cherry Ng <cherry92@gmail.com>
2021-01-21 18:04:59 +0800

    Only delete history expired.

--- background.js
+++ background.js
@@ -1,7 +1,12 @@
-chrome.history.onVisited.addListener(function (historyItem) {
-  chrome.history.deleteUrl({url: historyItem.url});
-});
+const CLEAR_HISTORY_ALARM = 'CLEAR_HISTORY_ALARM';
+const HISTORY_EXPIRE_MS = 15 * 60 * 1000;
 
-window.setInterval(function () {
-  chrome.history.deleteAll(function () {});
-}, 10 * 60 * 1000);
+chrome.alarms.create(CLEAR_HISTORY_ALARM, {periodInMinutes: 1});
+chrome.alarms.onAlarm.addListener(function (alarm) {
+  if (alarm.name == CLEAR_HISTORY_ALARM) {
+    chrome.history.deleteRange({
+      startTime: 0,
+      endTime: Date.now() - HISTORY_EXPIRE_MS,
+    }, function () {});
+  }
+});
